c1c48a9463ca51fef589ae624791b93bcd7f282b
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-04 07:04:55 -0500

    swagger agregado

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,21 @@ import (
 	"github.com/anderson2093/apiGolang/interfaces/routes"
 	"github.com/anderson2093/apiGolang/usecase"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Categories API
+// @version 1.0
+// @description API para gestionar categorías en una tienda virtual
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 	// Conectar a la base de datos
 	config.ConnectDatabase()
@@ -21,6 +34,11 @@ func main() {
 
 	// Configurar rutas
 	router := gin.Default()
+
+	// Ruta Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	// Registra las rutas de la API
 	routes.RegisterRoutes(router, controller)
 
 	// Iniciar servidor
